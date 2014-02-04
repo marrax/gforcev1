830629d3630ba980fd60bf5d307d0ce58179cc4a
marrax <marrax@gmail.com>
2014-02-04 17:07:35 +0000

    04-02-2014

--- backup.sh
+++ backup.sh
@@ -4,4 +4,6 @@ date=`date +%d-%m-%Y`
 rm -rf tmp/cache
 tar -cvzf ~/backups/gforcev1-$date.tar.gz .
 pg_dump gforcev1_development -U gforcev1  > ~/backups/gforcev1_development.dump.$date
-
+git add --all
+git commit -m $date 
+git push -u origin master
