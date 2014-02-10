3fda723b433b13cf21e738ee59eea2d407ee219c
marrax <marrax@gmail.com>
2014-02-10 19:57:37 +0000

    cap 2 stuff still

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -9,6 +9,10 @@ Spree.config do |config|
   # Example:
   # Uncomment to override the default site name.
    config.site_name = "Gforce-rc"
+   config.allow_ssl_in_staging = false
+   config.allow_ssl_in_production = false
+   config.allow_ssl_in_test = false
 end
 
 Spree.user_class = "Spree::User"
+
